2d6bc48e3e5b0776c9e3a1e35bc91d2c989e74b6
Kevin Castro <keacastroga@unal.edu.co>
2020-09-30 09:33:02 -0500

    VideoTest

---
+++ DisplayVideo.cpp
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <iostream>
+#include <opencv2/opencv.hpp>
+
+using namespace std;
+using namespace cv;
+int main(int argc, char** argv )
+{
+    if ( argc != 2 )
+    {
+        printf("usage: DisplayVideo.out <Video_Path>\n");
+        return -1;
+    }
+    VideoCapture cap(argv[1]);
+    if(!cap.isOpened()){
+	    cout << "Error opening video stream or file" << endl;
+	    return -1;
+	} 
+    while(1){
+        Mat frame;
+        cap >> frame;
+        if(frame.empty())
+            break;
+        imshow("Frame", frame);
+        char c=(char)waitKey(25);
+        if(c==27)
+            break;
+    }
+    cap.release();
+    destroyAllWindows();
+    return 0;
+}
---
+++ README.md
@@ -0,0 +1,10 @@
+### Compilar
+```
+cmake .
+make
+```
+### Correr
+```
+./DisplayVideo.out "path_to_video.mp4"
+```
+
