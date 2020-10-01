bcc7d27687bcfdeb5d484b675ae5d6dd8df79779
Kevin Castro <keacastroga@unal.edu.co>
2020-10-01 10:46:12 -0500

    Make video

--- VideoReScale.cpp
+++ VideoReScale.cpp
@@ -5,6 +5,8 @@
 
 using namespace std;
 using namespace cv;
+void videoSave(vector<Mat> frames, double fps, int fourcc, Size S);
+
 int main(int argc, char** argv )
 {
     if ( argc != 2 )
@@ -17,7 +19,11 @@ int main(int argc, char** argv )
     if(!cap.isOpened()){
 	    cout << "Error opening video stream or file" << endl;
 	    return -1;
-	} 
+	}
+    double fps = cap.get(CAP_PROP_FPS);
+    int fourcc = static_cast<int>(cap.get(CAP_PROP_FOURCC)); 
+
+
 
     //Pasar frames del video a vector de Mat
     vector<Mat> frames;
@@ -30,7 +36,7 @@ int main(int argc, char** argv )
     }
 
     //mostrando video por medio del vector de frames
-    int i = 0;
+/*     int i = 0;
     while(1){
         imshow("Frame", frames[i]);
         i++;
@@ -38,8 +44,31 @@ int main(int argc, char** argv )
         char c=(char)waitKey(25);
         if(c==27)
             break;
-    }
+    } */
+
+    //TODO: funcion para cambiar el tamaño del video
+
+    // Resolucion del video origianl, debe ser cambiado para obtener nueve resolucion
+    Size S = Size((int) cap.get(CAP_PROP_FRAME_WIDTH),    
+                  (int) cap.get(CAP_PROP_FRAME_HEIGHT));
+    videoSave(frames, fps, fourcc, S);
     cap.release();
     destroyAllWindows();
     return 0;
+}
+
+void videoSave(vector<Mat> frames, double fps, int fourcc, Size S){
+    cout << "Guardando video..." << endl;
+    VideoWriter out;
+    out.open("out.mp4", fourcc, fps, S);
+    if (!out.isOpened())
+    {
+        cout  << "Could not open the output video for write: " << endl;
+        return;
+    }
+    for (Mat frame : frames)
+    {
+        out << frame;
+    }
+    out.release();
 }
