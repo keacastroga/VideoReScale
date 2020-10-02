f022825d8b68a5c7d52b584ca3f596a2da6825d2
Kevin Castro <keacastroga@unal.edu.co>
2020-10-02 00:37:24 -0500

    video resizes

--- VideoReScale.cpp
+++ VideoReScale.cpp
@@ -1,74 +1,52 @@
 #include <stdio.h>
 #include <iostream>
+#include <string>
 #include <vector>
 #include <opencv2/opencv.hpp>
 
 using namespace std;
 using namespace cv;
-void videoSave(vector<Mat> frames, double fps, int fourcc, Size S);
 
 int main(int argc, char** argv )
 {
-    if ( argc != 2 )
+    if ( argc != 3 )
     {
-        printf("usage: VideoReScale.out <Video_Path>\n");
+        printf("usage: VideoReScale.out <Video_Path> <scale factor>\n");
         return -1;
     }
-    //Cargando video
+
     VideoCapture cap(argv[1]);
     if(!cap.isOpened()){
 	    cout << "Error opening video stream or file" << endl;
 	    return -1;
 	}
+
+    VideoWriter out;
+    double factor = stod(argv[2]);
     double fps = cap.get(CAP_PROP_FPS);
     int fourcc = static_cast<int>(cap.get(CAP_PROP_FOURCC)); 
-
-
-
-    //Pasar frames del video a vector de Mat
-    vector<Mat> frames;
-    while(1){
-        Mat frame;
-        cap >> frame;
-        if(frame.empty())
-            break;
-        frames.push_back(frame);
-    }
-
-    //mostrando video por medio del vector de frames
-/*     int i = 0;
-    while(1){
-        imshow("Frame", frames[i]);
-        i++;
-        frames.pop_back();
-        char c=(char)waitKey(25);
-        if(c==27)
-            break;
-    } */
-
-    //TODO: funcion para cambiar el tamaño del video
-
-    // Resolucion del video origianl, debe ser cambiado para obtener nueve resolucion
-    Size S = Size((int) cap.get(CAP_PROP_FRAME_WIDTH),    
-                  (int) cap.get(CAP_PROP_FRAME_HEIGHT));
-    videoSave(frames, fps, fourcc, S);
-    cap.release();
-    destroyAllWindows();
-    return 0;
-}
-
-void videoSave(vector<Mat> frames, double fps, int fourcc, Size S){
-    cout << "Guardando video..." << endl;
-    VideoWriter out;
+    Size S = Size(((int) cap.get(CAP_PROP_FRAME_WIDTH)) * factor,    
+                  ((int) cap.get(CAP_PROP_FRAME_HEIGHT)) * factor);
     out.open("out.mp4", fourcc, fps, S);
     if (!out.isOpened())
     {
         cout  << "Could not open the output video for write: " << endl;
-        return;
+        return -1;
     }
-    for (Mat frame : frames)
-    {
-        out << frame;
+    
+    Size s0 = Size();
+    while(1){
+        Mat frame;
+        Mat outFrame;
+        cap >> frame;
+        if(frame.empty())
+            break;
+        resize(frame,outFrame,s0,factor,factor);        
+        out << outFrame;   
+        frame.release();
+        outFrame.release();    
     }
     out.release();
+    cap.release();
+    return 0;
 }
