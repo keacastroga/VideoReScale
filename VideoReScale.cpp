9333653b09030d07c3c765bfccd892a3c370a3e1
Kevin Castro <keacastroga@unal.edu.co>
2020-10-20 23:25:26 -0500

    OpenMP

--- VideoReScale.cpp
+++ VideoReScale.cpp
@@ -3,13 +3,17 @@
 #include <string>
 #include <vector>
 #include <opencv2/opencv.hpp>
+#include <sys/time.h>
 
 using namespace std;
 using namespace cv;
 
+#define THREADS 8
+
 int rows, cols, channels, rowLen, newRows, newCols, newRowLen, origRow, origCol;
 
 void sequentialScale(uchar *pixels, uchar *newPixels);
+void openMPScale(uchar *pixels, uchar *newPixels);
 
 int main(int argc, char **argv)
 {
@@ -41,6 +45,9 @@ int main(int argc, char **argv)
     }
 
     Size s0 = Size();
+
+    struct timeval tval_before, tval_after, tval_result;
+    gettimeofday(&tval_before, NULL);
     while (1)
     {
         Mat frame;
@@ -56,6 +63,7 @@ int main(int argc, char **argv)
         newCols = cols * factor;
         newRowLen = channels * newCols;
         unsigned char *newPixels = (uchar *)malloc(sizeof(uchar) * newCols * channels * newRows);
+        //openMPScale(pixels, newPixels);
         sequentialScale(pixels, newPixels);
         Mat outFrame(newRows, newCols, CV_8UC3, newPixels);
         out << outFrame;
@@ -63,6 +71,9 @@ int main(int argc, char **argv)
         outFrame.release();
         free(newPixels);
     }
+    gettimeofday(&tval_after, NULL);
+    timersub(&tval_after, &tval_before, &tval_result);
+    printf("Time elapsed: %ld.%06ld\n", (long int)tval_result.tv_sec, (long int)tval_result.tv_usec);
     out.release();
     cap.release();
     return 0;
@@ -70,7 +81,7 @@ int main(int argc, char **argv)
 
 void sequentialScale(uchar *pixels, uchar *newPixels)
 {
-    unsigned char pixel1, pixel2, pixel3, pixel4;
+    unsigned char *pixel1, *pixel2, *pixel3, *pixel4;
     for (int k = 0; k < channels; k++)
     {
         for (int scaledRow = 0; scaledRow < newRows; scaledRow++)
@@ -79,11 +90,35 @@ void sequentialScale(uchar *pixels, uchar *newPixels)
             for (int scaledCol = 0; scaledCol < newCols; scaledCol++)
             {
                 origCol = scaledCol * 2;
-                pixel1 = *(pixels + origCol * channels + origRow * rowLen + k);
-                pixel2 = *(pixels + origCol * channels + (origRow + 1) * rowLen + k);
-                pixel3 = *(pixels + (origCol + 1) * channels + origRow * rowLen + k);
-                pixel4 = *(pixels + (origCol + 1) * channels + (origRow + 1) * rowLen + k);
-                *(newPixels + scaledCol * channels + scaledRow * newRowLen + k) = (pixel1 + pixel2 + pixel3 + pixel4) / 4;
+                uchar *pixel1 = pixels + origCol * channels + origRow * rowLen + k;
+                uchar *pixel2 = pixel1 + channels + k;
+                uchar *pixel3 = pixels + (origCol + 1) * channels + origRow * rowLen + k;
+                uchar *pixel4 = pixel3 + channels + k;
+                *(newPixels + scaledCol * channels + scaledRow * newRowLen + k) = (*pixel1 + *pixel2 + *pixel3 + *pixel4) / 4;
+            }
+        }
+    }
+}
+
+void openMPScale(uchar *pixels, uchar *newPixels)
+{
+    for (int k = 0; k < channels; k++)
+    {
+#pragma omp parallel num_threads(THREADS) private(origRow, origCol)
+        {
+#pragma omp for
+            for (int scaledRow = 0; scaledRow < newRows; scaledRow++)
+            {
+                origRow = scaledRow * 2;
+                for (int scaledCol = 0; scaledCol < newCols; scaledCol++)
+                {
+                    origCol = scaledCol * 2;
+                    uchar *pixel1 = pixels + origCol * channels + origRow * rowLen + k;
+                    uchar *pixel2 = pixel1 + channels + k;
+                    uchar *pixel3 = pixels + (origCol + 1) * channels + origRow * rowLen + k;
+                    uchar *pixel4 = pixel3 + channels + k;
+                    *(newPixels + scaledCol * channels + scaledRow * newRowLen + k) = (*pixel1 + *pixel2 + *pixel3 + *pixel4) / 4;
+                }
             }
         }
     }
