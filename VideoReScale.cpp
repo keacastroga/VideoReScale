5f406f3c28ad180e7573edf48bd64d1c099abb5d
Kevin Castro <keacastroga@unal.edu.co>
2020-10-20 22:15:01 -0500

    Sequential

--- VideoReScale.cpp
+++ VideoReScale.cpp
@@ -7,6 +7,10 @@
 using namespace std;
 using namespace cv;
 
+int rows, cols, channels, rowLen, newRows, newCols, newRowLen, origRow, origCol;
+
+void sequentialScale(uchar *pixels, uchar *newPixels);
+
 int main(int argc, char **argv)
 {
     if (argc != 4)
@@ -44,42 +48,43 @@ int main(int argc, char **argv)
         if (frame.empty())
             break;
         unsigned char *pixels = frame.data;
-        int rows = frame.rows;
-        int cols = frame.cols;
-        int channels = frame.step[1];
-        int rowLen = channels * cols;
-
-        int newRows = rows*factor;
-        int newCols = cols*factor;
-        int newRowLen = channels * newCols;
+        rows = frame.rows;
+        cols = frame.cols;
+        channels = frame.step[1];
+        rowLen = channels * cols;
+        newRows = rows * factor;
+        newCols = cols * factor;
+        newRowLen = channels * newCols;
         unsigned char *newPixels = (uchar *)malloc(sizeof(uchar) * newCols * channels * newRows);
-        unsigned char pixel1, pixel2, pixel3, pixel4;
-        int origRow, origCol;
-        for (int k = 0; k < channels; k++)
-        {
-            for (int scaledRow = 0; scaledRow < newRows; scaledRow++)
-            {
-                origRow = scaledRow*2;
-                for (int scaledCol = 0; scaledCol < newCols; scaledCol++)
-                {
-                    origCol = scaledCol*2;
-                    pixel1 = *(pixels + origCol * channels + origRow * rowLen + k);
-                    pixel2 = *(pixels + origCol * channels + (origRow+1) * rowLen + k);
-                    pixel3 = *(pixels + (origCol+1) * channels + origRow * rowLen + k);
-                    pixel4 = *(pixels + (origCol+1) * channels + (origRow+1) * rowLen + k);
-                    *(newPixels + scaledCol * channels + scaledRow * newRowLen + k) = (pixel1 + pixel2 + pixel3 + pixel4) / 4;
-                }
-            }
-        }
-
+        sequentialScale(pixels, newPixels);
         Mat outFrame(newRows, newCols, CV_8UC3, newPixels);
         out << outFrame;
         frame.release();
         outFrame.release();
         free(newPixels);
-        
     }
     out.release();
     cap.release();
     return 0;
+}
+
+void sequentialScale(uchar *pixels, uchar *newPixels)
+{
+    unsigned char pixel1, pixel2, pixel3, pixel4;
+    for (int k = 0; k < channels; k++)
+    {
+        for (int scaledRow = 0; scaledRow < newRows; scaledRow++)
+        {
+            origRow = scaledRow * 2;
+            for (int scaledCol = 0; scaledCol < newCols; scaledCol++)
+            {
+                origCol = scaledCol * 2;
+                pixel1 = *(pixels + origCol * channels + origRow * rowLen + k);
+                pixel2 = *(pixels + origCol * channels + (origRow + 1) * rowLen + k);
+                pixel3 = *(pixels + (origCol + 1) * channels + origRow * rowLen + k);
+                pixel4 = *(pixels + (origCol + 1) * channels + (origRow + 1) * rowLen + k);
+                *(newPixels + scaledCol * channels + scaledRow * newRowLen + k) = (pixel1 + pixel2 + pixel3 + pixel4) / 4;
+            }
+        }
+    }
 }
