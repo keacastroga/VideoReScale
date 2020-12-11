dcd8ef25fb3bb5fd4c4c054e3580024f5a5089ae
Kevin Castro <keacastroga@unal.edu.co>
2020-12-11 00:09:53 -0500

    mpi done

--- VideoReScale.cpp
+++ VideoReScale.cpp
@@ -11,89 +11,118 @@ using namespace cv;
 
 int rows, cols, channels, rowLen, newRows, newCols, newRowLen, origRow;
 
-unsigned char *pixel1, *pixel2, *pixel3, *pixel4, *newPixel;
-
-void sequentialScale(uchar *pixels, uchar *newPixels);
-void openMPScale(uchar *pixels, uchar *newPixels);
+void sequentialScale(uchar *pixels, uchar *newPixels, int ID, int numprocs);
 
 int main(int argc, char **argv)
 {
     int rank, size;
-    MPI_Init( &argc, &argv );
+    MPI_Init(&argc, &argv);
     MPI_Comm_size(MPI_COMM_WORLD, &size);
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    if (argc != 4)
+    {
+        printf("usage: VideoReScale.out <Video_Path> <scale factor> <Output_path>\n");
+        return -1;
+    }
+
+    VideoCapture cap(argv[1]);
+    if (!cap.isOpened())
+    {
+        cout << "Error opening video stream or file" << endl;
+        return -1;
+    }
+
+    VideoWriter out;
+    Mat frame;
+    Mat outFrame;
+    double factor = stod(argv[2]);
+    double fps = cap.get(CAP_PROP_FPS);
+    int frames = cap.get(CAP_PROP_FRAME_COUNT);
+
+    int fourcc = VideoWriter::fourcc('H', '2', '6', '4');
+    rows = (int)cap.get(CAP_PROP_FRAME_HEIGHT);
+    cols = (int)cap.get(CAP_PROP_FRAME_WIDTH);
+    channels = 3;
+    rowLen = channels * cols;
+    newRows = rows * factor;
+    newCols = cols * factor;
+    newRowLen = channels * newCols;
+
+    Size S = Size(newCols, newRows);
+
+    size_t sizeNew = sizeof(uchar) * newCols * channels * newRows;
+    size_t sizeOrig = sizeof(uchar) * cols * channels * rows;
+    unsigned char *newPixels = (uchar *)malloc(sizeNew);
+    unsigned char *newPixelsPart = (uchar *)malloc(sizeNew);
+    unsigned char *pixels = (uchar *)malloc(sizeOrig);
+
     if (rank == 0)
     {
-        printf("Yes Program from process %d of %d\n", rank+1, size );
-        if (argc != 4)
-        {
-            printf("usage: VideoReScale.out <Video_Path> <scale factor> <Output_path>\n");
-            return -1;
-        }
-
-        VideoCapture cap(argv[1]);
-        if (!cap.isOpened())
-        {
-            cout << "Error opening video stream or file" << endl;
-            return -1;
-        }
-
-        VideoWriter out;
-        double factor = stod(argv[2]);
-        double fps = cap.get(CAP_PROP_FPS);
-        //int fourcc = static_cast<int>(cap.get(CAP_PROP_FOURCC));
-        int fourcc = VideoWriter::fourcc('H', '2', '6', '4');
-        rows = (int)cap.get(CAP_PROP_FRAME_HEIGHT);
-        cols = (int)cap.get(CAP_PROP_FRAME_WIDTH);
-        channels = 3;
-        rowLen = channels * cols;
-        newRows = rows * factor;
-        newCols = cols * factor;
-        newRowLen = channels * newCols;
-
-        Size S = Size(newCols, newRows);
-
-        unsigned char *newPixels = (uchar *)malloc(sizeof(uchar) * newCols * channels * newRows);
-
         out.open(argv[3], fourcc, fps, S);
         if (!out.isOpened())
         {
             cout << "Could not open the output video for write: " << endl;
             return -1;
         }
-
-        Mat frame;
-        Mat outFrame(newRows, newCols, CV_8UC3, newPixels);
-        struct timeval tval_before, tval_after, tval_result;
-        gettimeofday(&tval_before, NULL);
-        while (1)
+        outFrame = Mat(newRows, newCols, CV_8UC3, newPixels);
+    }
+    struct timeval tval_before, tval_after, tval_result;
+    gettimeofday(&tval_before, NULL);
+    int i = 0;
+    int j, tag = 1;
+    MPI_Status status;
+    while (i < frames)
+    {
+        if (rank == 0)
         {
             cap >> frame;
             if (frame.empty())
+            {
                 break;
-            sequentialScale(frame.data, newPixels);
-            out << outFrame;
+            }
+            pixels = frame.data;
+            for (j = 1; j < size; j++)
+                MPI_Send(pixels, sizeOrig, MPI_UNSIGNED_CHAR, j, tag, MPI_COMM_WORLD);
+            cout << "frame: " << i << "/" << frames << "\r";
         }
-        gettimeofday(&tval_after, NULL);
-        timersub(&tval_after, &tval_before, &tval_result);
+        else
+        {
+            MPI_Recv(pixels, sizeOrig, MPI_UNSIGNED_CHAR, 0, tag, MPI_COMM_WORLD, &status);
+        }
+        sequentialScale(pixels, newPixelsPart, rank, size);
+        MPI_Reduce(newPixelsPart, newPixels, sizeNew, MPI_UNSIGNED_CHAR, MPI_SUM, 0, MPI_COMM_WORLD);
+        if (rank == 0)
+            out << outFrame;
+        i++;        
+    }
+    gettimeofday(&tval_after, NULL);
+    timersub(&tval_after, &tval_before, &tval_result);
+    
+    if (rank == 0)
+    {
         printf("Time elapsed: %ld.%06ld\n", (long int)tval_result.tv_sec, (long int)tval_result.tv_usec);
-        free(newPixels);
         frame.release();
         outFrame.release();
         out.release();
-        cap.release();
-    }else{
-        printf("No Program from process %d of %d\n", rank+1, size );
     }
+    else
+    {
+        free(pixels);
+    }
+    cap.release();
     MPI_Finalize();
     return 0;
 }
 
-void sequentialScale(uchar *pixels, uchar *newPixels)
+void sequentialScale(uchar *pixels, uchar *newPixels, int ID, int numprocs)
 {
-    for (int scaledRow = 0; scaledRow < newRows; scaledRow++)
+    int start, end;
+    start = (newRows / numprocs) * ID;
+    end = start + ((newRows / numprocs));
+    for (int scaledRow = start; scaledRow < end; scaledRow++)
     {
         origRow = scaledRow * 2 * rowLen;
+        uchar *pixel1, *pixel2, *pixel3, *pixel4, *newPixel;
         for (int scaledCol = 0; scaledCol < newCols; scaledCol++)
         {
             pixel1 = pixels + scaledCol * 2 * channels + origRow;
